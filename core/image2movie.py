e3899d9e5a7337f000d5e60c4a107e9b465c8d72
杜健 <sampson@dujiandebijibendiannao.local>
2024-10-31 23:36:30 +0800

    1

--- core/image2movie.py
+++ core/image2movie.py
@@ -1,14 +1,17 @@
 import random
 import datetime
 import os
-
+from pypinyin import pinyin, Style
 from moviepy.editor import AudioFileClip, ImageClip, TextClip, concatenate_videoclips, CompositeVideoClip
-from moviepy.video.tools.subtitles import SubtitlesClip
+
+# os.environ['IMAGEIO_FFMPEG_EXE'] = 'ffmpeg'
 
 """
 定义Scene类，对应每一幕，使用图片路径、音频路径、文本字符串创建一个Scene对象。
 调用generate_movie_from_scenes生成视频，参数为Scene列表。
 """
+
+withPinYin = True
 
 
 class Scene:
@@ -16,46 +19,44 @@ class Scene:
         self.image_file = image_file
         self.audio_file = audio_file
         self.text = text
+        self.pinyin_text = self.generate_pinyin(text) if withPinYin == True else " "
 
+    def generate_pinyin(self, text):
+        # 将中文文本转换为拼音
+        pinyin_text = ' '.join([' '.join(item) for item in pinyin(text, style=Style.TONE)])
+        return pinyin_text
 
 def generate_movie_from_scenes(scenes):
-
-    # 视频剪辑列表
     video_clips = []
 
     for scene in scenes:
-
-        # 创建音频剪辑
         audio_clip = AudioFileClip(scene.audio_file)
+        duration = audio_clip.duration
 
-        # 将图片和字幕的时长设置为音频的时长
-        duration = audio_clip.duration  
-
-        # 创建图片剪辑
         image_clip = ImageClip(scene.image_file)
         image_clip.duration = duration
 
         # 创建字幕剪辑
         text_clip = TextClip(scene.text, font='resource/FZSTK.TTF', fontsize=24, color="white")
-        text_clip = text_clip.set_position(("center","center"))
+        text_clip = text_clip.set_position(("center", "center"))
         text_clip.duration = duration
 
-        # 将图片，音频和字幕剪辑在一起
-        vedio_clip = CompositeVideoClip([image_clip, text_clip]).set_audio(audio_clip)
+        pinyin_clip = TextClip(scene.pinyin_text, font='resource/FZSTK.TTF', fontsize=23, color="white")
+        pinyin_clip = pinyin_clip.set_position(("center", 0.51), True) 
+        pinyin_clip.duration = duration
+        pinyin_clip.duration = duration
+
+        vedio_clip = CompositeVideoClip([image_clip, text_clip, pinyin_clip]).set_audio(audio_clip)
         vedio_clip.duration = duration
-    
-        # 添加到视频剪辑列表
+
         video_clips.append(vedio_clip)
 
-    # 合并所有剪辑
     final_video = concatenate_videoclips(video_clips, method="compose")
 
-    # 导出视频
-    random_movie_name = '{0:%Y%m%d%H%M%S%f}'.format(datetime.datetime.now()) + ''.join([str(random.randint(1,10)) for i in range(5)])
+    random_movie_name = '{0:%Y%m%d%H%M%S%f}'.format(datetime.datetime.now()) + ''.join([str(random.randint(1, 10)) for _ in range(5)])
     movie_path = os.path.join("temp", "movie", random_movie_name + ".mp4")
     final_video.write_videofile(movie_path, codec="libx264", fps=24)
     return movie_path
-
 
 def generate_movie_from_resource_list(image_list, audio_list, text_list):
     """
@@ -79,10 +80,8 @@ def generate_movie_from_resource_list(image_list, audio_list, text_list):
 
     return generate_movie_from_scenes(scene_list)
 
-
 if __name__ == '__main__':
     image_list = ["temp/image/2024103119464248885723965.jpg", "temp/image/2024103119464870756483389.jpg"]
     audio_list = ["temp/audio/2024103119464248686089455.mp3", "temp/audio/2024103119465599362594685.mp3"]
-    # text_list = ["temp/subtitle/1.txt", "temp/subtitle/2.txt"]
     text_list = ["小兔子丽丽的故事", "大兔子宝宝的故事"]
     generate_movie_from_resource_list(image_list, audio_list, text_list)
